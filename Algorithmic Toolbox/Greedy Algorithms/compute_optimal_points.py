ca0e859efe098935de428f6be6f3fd2ff6667dcd
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-01-13 17:02:34 +0400

    Update compute_optimal_points.py

--- Algorithmic Toolbox/Greedy Algorithms/compute_optimal_points.py
+++ Algorithmic Toolbox/Greedy Algorithms/compute_optimal_points.py
@@ -9,5 +9,4 @@ def compute_optimal_points(segments):
         if position[-1] >= segments[i][0]:
             continue
         position.append(segments[i][1])
-    print(position)
     return len(position)
