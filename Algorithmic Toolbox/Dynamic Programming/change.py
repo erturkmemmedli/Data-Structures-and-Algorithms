c032d169bd2b4037fd0bacca6f9a4d5be040c9e0
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-01-13 17:35:24 +0400

    Create change.py

---
+++ Algorithmic Toolbox/Dynamic Programming/change.py
@@ -0,0 +1,11 @@
+def change(money):
+    coins = [1, 3, 4]
+    MinimumCoins = [0] * (money + 1)
+    for i in range(1, money + 1):
+        MinimumCoins[i] = float('inf')
+        for j in range(0, len(coins)):
+            if i >= coins[j]:
+                min_value = MinimumCoins[i-coins[j]] + 1
+                if min_value < MinimumCoins[i]:
+                    MinimumCoins[i] = min_value
+    return MinimumCoins[money]
