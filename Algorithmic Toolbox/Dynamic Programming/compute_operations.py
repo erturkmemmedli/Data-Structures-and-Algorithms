fbb73a4e1e3acbf9d548a7f796f9b7739c2fa94f
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-01-13 17:38:39 +0400

    Create compute_operations.py

---
+++ Algorithmic Toolbox/Dynamic Programming/compute_operations.py
@@ -0,0 +1,32 @@
+def compute_operations(n):
+    assert 1 <= n <= 10 ** 6
+    counts = [0] * (n + 1)
+    if n == 1:
+        return 0
+    for i in range(2, n + 1):
+        result1 = counts[i - 1] + 1
+        if i % 2 == 0:
+            result2 = counts[i // 2] + 1
+        else:
+            result2 = float('inf')
+        if i % 3 == 0:
+            result3 = counts[i // 3] + 1
+        else:
+            result3 = float('inf')
+        counts[i] = min(result1, result2, result3)
+        
+    output = [n]
+    k = n
+    while True:
+        if counts[k] == counts[k - 1] + 1:
+            k = k - 1
+            output.insert(0, k)
+        elif k % 2 == 0 and counts[k] == counts[k // 2] + 1:
+            k = k // 2
+            output.insert(0, k)
+        elif k % 3 == 0 and counts[k] == counts[k // 3] + 1:
+            k = k // 3
+            output.insert(0, k) 
+        if k == 1:
+            break
+    return output
