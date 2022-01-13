e8dfb4661af5f26ca20e6125d33ab532fed81e83
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-01-13 17:23:29 +0400

    Update majority_element.py

--- Algorithmic Toolbox/Divide and Conquer Algorithms/majority_element.py
+++ Algorithmic Toolbox/Divide and Conquer Algorithms/majority_element.py
@@ -1,8 +1,6 @@
 def majority_element(elements):
     assert 1 <= len(elements) <= 10 ** 5
-    l = 0
-    r = len(elements) - 1
-    randomized_quick_sort(elements, l, r)
+    elements.sort()
     n = len(elements)
     for i in range(0, (n+1)//2):
         if elements[i] == elements[i + n//2]:
