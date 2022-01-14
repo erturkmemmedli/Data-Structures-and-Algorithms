2fb3d8faa8b229449c2d127f46991f874d5ccf68
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-01-14 11:43:54 +0400

    Add files via upload

---
+++ Algorithmic Toolbox/Algorithmic Warm Up/fibonacci_number_again.py
@@ -0,0 +1,13 @@
+def fibonacci_number_again(n, m):
+    assert 0 <= n <= 10 ** 18 and 2 <= m <= 10 ** 6
+    F = [0,1]
+    for i in range(2,n+1):
+        F.append((F[i-1]+F[i-2]) % m)
+        if F[i-1] == 0 and F[i] == 1:
+            F = F[:-2]
+            break
+    return F[n % len(F)]
+
+if __name__ == '__main__':
+    input_n, input_m = map(int, input().split())
+    print(fibonacci_number_again(input_n, input_m))
