2c855568202a0c4648e51e274f8bd3b9c11ff88f
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-01-14 23:21:31 +0400

    Delete Algorithmic Toolbox/Divide and Conquer Algorithms directory

--- Algorithmic Toolbox/Divide and Conquer Algorithms/majority_element.py
+++
@@ -1,8 +0,0 @@
-def majority_element(elements):
-    assert 1 <= len(elements) <= 10 ** 5
-    elements.sort()
-    n = len(elements)
-    for i in range(0, (n+1)//2):
-        if elements[i] == elements[i + n//2]:
-            return 1
-    return 0
