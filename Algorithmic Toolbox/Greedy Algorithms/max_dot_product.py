5ce6ce8f53c4d2529b160830c2e68d3cead79ae7
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-01-14 20:46:11 +0400

    Delete Algorithmic Toolbox/Greedy Algorithms directory

--- Algorithmic Toolbox/Greedy Algorithms/max_dot_product.py
+++
@@ -1,7 +0,0 @@
-def max_dot_product(first_sequence, second_sequence):
-    assert len(first_sequence) == len(second_sequence)
-    assert len(first_sequence) <= 10 ** 3
-    assert all(0 <= f <= 10 ** 5 for f in first_sequence)
-    assert all(0 <= s <= 10 ** 5 for s in second_sequence)
-    
-    return sum([i * j for i,j in zip(sorted(first_sequence), sorted(second_sequence))])
--- Algorithmic Toolbox/Greedy Algorithms/money_change.py
+++
@@ -1,3 +0,0 @@
-def money_change(money):
-    assert 0 <= money <= 10 ** 3
-    return int(money / 10) + int((money % 10) / 5) + int(money % 5)
