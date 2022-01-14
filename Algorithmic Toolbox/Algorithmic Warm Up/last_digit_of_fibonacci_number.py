d1ad57bc36b0c86582d8cd5c04e1adb3c74b3590
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-01-14 11:42:49 +0400

    Delete Algorithmic Toolbox/Algorithmic Warm Up directory

--- Algorithmic Toolbox/Algorithmic Warm Up/last_digit_of_fibonacci_number.py
+++
@@ -1,9 +0,0 @@
-def last_digit_of_fibonacci_number(n):
-    assert 0 <= n <= 10 ** 7
-    F = [0,1]
-    for i in range(2,n+1):
-        F.append((F[i-1]+F[i-2]) % 10)
-        if F[i-1] == 0 and F[i] == 1:
-            F = F[:-2]
-            break
-    return F[n % len(F)]
--- Algorithmic Toolbox/Algorithmic Warm Up/sum_of_two_digits.py
+++
@@ -1,3 +0,0 @@
-def sum_of_two_digits(first_digit, second_digit):
-    assert 0 <= first_digit <= 9 and 0 <= second_digit <= 9
-    return first_digit + second_digit
