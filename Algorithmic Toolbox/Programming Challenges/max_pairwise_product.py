3e63e824e1cb63cf4fbf1645dc981b2268fd3a52
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-01-14 11:36:11 +0400

    Add files via upload

---
+++ Algorithmic Toolbox/Programming Challenges/max_pairwise_product.py
@@ -0,0 +1,20 @@
+def max_pairwise_product(A):
+    assert 2 <= len(A) <= 2 * 10 ** 5
+    assert all(0 <= x <= 2 * 10 ** 5 for x in A)
+    if len(A) == 2:
+        return A[0] * A[1]
+    max1 = 0
+    max2 = 0
+    for i in range(len(A)):
+        if A[i] > max1:
+            max2 = max1
+            max1 = A[i]
+        elif A[i] > max2:
+            max2 = A[i]
+    return max1 * max2
+
+if __name__ == '__main__':
+    n = int(input())
+    input_numbers = list(map(int, input().split()))
+    assert len(input_numbers) == n
+    print(max_pairwise_product(input_numbers))
