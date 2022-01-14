a315620b55a374c179c90c0d6b28729c012c2546
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-01-14 11:43:09 +0400

    Add files via upload

---
+++ Algorithmic Toolbox/Algorithmic Warm Up/last_digit_of_the_sum_of_squares_of_fibonacci_numbers.py
@@ -0,0 +1,13 @@
+def last_digit_of_the_sum_of_squares_of_fibonacci_numbers(n):
+    assert 0 <= n <= 10 ** 18
+    F = [0,1]
+    for i in range(2,n+1):
+        F.append((F[i-1]+F[i-2]) % 10)
+        if F[i-1] == 0 and F[i] == 1:
+            F = F[:-2]
+            break
+    return (F[n % len(F)] * (F[n % len(F)] + F[(n % len(F)) - 1])) % 10
+
+if __name__ == '__main__':
+    input_n = int(input())
+    print(last_digit_of_the_sum_of_squares_of_fibonacci_numbers(input_n))
