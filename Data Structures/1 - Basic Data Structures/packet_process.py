755934f7bb2257e67e41a4b16e50ac9b1a915b24
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-03-14 20:26:11 +0400

    Update packet_process.py

--- Data Structures/1 - Basic Data Structures/packet_process.py
+++ Data Structures/1 - Basic Data Structures/packet_process.py
@@ -1,8 +1,4 @@
-from collections import namedtuple
 from collections import deque
-
-Request = namedtuple("Request", ["arrived_at", "time_to_process"])
-Response = namedtuple("Response", ["was_dropped", "started_at"])
 
 class Buffer:
     def __init__(self, size):
@@ -11,12 +7,15 @@ class Buffer:
 
     def process(self, request):
         max_size = self.size
+        temp = 0
         
         while self.size >= 0 and self.size <= max_size:
             was_dropped = False
             
             if len(self.finish_time) > 0:
-                if self.finish_time[0] <= request[0]:
+                if self.finish_time[0] <= request.arrived_at:
+                    if len(self.finish_time) == 1:
+                        temp = self.finish_time[-1]
                     self.finish_time.popleft()
                     self.size += 1
                     
@@ -25,35 +24,48 @@ class Buffer:
                 
             if len(self.finish_time) > 0:
                 started_at = self.finish_time[-1]
-                self.finish_time.append(self.finish_time[-1] + request[1])
+                self.finish_time.append(self.finish_time[-1] + request.time_to_process)
                 self.size -= 1
                 return Response(was_dropped, started_at)
             
             if len(self.finish_time) == 0:
-                started_at = request[0]
-                self.finish_time.append(request[1])
+                started_at = request.arrived_at
+                self.finish_time.append(temp + request.time_to_process)
                 self.size -= 1
                 return Response(was_dropped, started_at)
             
         return Response(True, -1)
 
-def main():
-    buffer_size, n_requests = map(int, input().split())
-    
+class Request:
+    def __init__(self, arrived_at, time_to_process):
+        self.arrived_at = arrived_at
+        self.time_to_process = time_to_process
+
+class Response:
+    def __init__(self, was_dropped, started_at):
+        self.was_dropped = was_dropped
+        self.started_at = started_at
+
+def requests(n_requests):
     requests = []
-    responses = []
-    
     for i in range(n_requests):
         arrived_at, time_to_process = map(int, input().split())
         requests.append(Request(arrived_at, time_to_process))
-        
-    buffer = Buffer(buffer_size)
+    return requests
 
+def responses(buffer, requests):
+    responses = []
     for request in requests:
         responses.append(buffer.process(request))
-    
+    return responses
+
+def result(responses):
     for response in responses:
         print(response.started_at if not response.was_dropped else -1)
 
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    buffer_size, n_requests = map(int, input().split())
+    requests = requests(n_requests)
+    buffer = Buffer(buffer_size)
+    responses = responses(buffer, requests)
+    result(responses)
