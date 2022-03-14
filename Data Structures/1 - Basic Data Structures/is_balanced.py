126c61ec347cf378b865a8d127fee36b2af60a46
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-03-14 17:17:53 +0400

    Update is_balanced.py

--- Data Structures/1 - Basic Data Structures/is_balanced.py
+++ Data Structures/1 - Basic Data Structures/is_balanced.py
@@ -3,19 +3,19 @@ def isBalanced(s):
     for i in range(len(s)):
         if s[i] in ['[','{','(']:
             stack.append(s[i])
-            stack.append(i)
+            stack.append(i + 1)
             #print('stack', stack)
         elif s[i] not in [')','}',']']:
             continue
         elif len(stack) == 0:
             #print(f'closing bracket {s[i]} at index {i}')
-            return i
+            return i + 1
         else:
             index = stack.pop()
             x = stack.pop()
             if (x == '(' and s[i] != ')') or (x == '{' and s[i] != '}') or (x == '[' and s[i] != ']'):
                 #print(f'closing bracket {s[i]} at index {i} dismatches with opening bracket {x} at index {index}')
-                return i
+                return i + 1
             #print('match:', x, s[i])
             #print('stack', stack)
     if len(stack) == 0:
