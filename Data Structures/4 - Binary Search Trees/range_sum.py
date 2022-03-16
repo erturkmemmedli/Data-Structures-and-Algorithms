b4dbc34e12338a7ca32574e78e509b69d5648111
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-03-16 17:19:11 +0400

    Update range_sum.py

--- Data Structures/4 - Binary Search Trees/range_sum.py
+++ Data Structures/4 - Binary Search Trees/range_sum.py
@@ -1,210 +1,191 @@
-class Node:
-    def __init__(self, key):
-        self.key = key
-        self.right = None
-        self.left = None
-        self.parent = None
-        self.height = 1
-        
-def Find(key, root):
-    if root == None:
-        return root
-    if key < root.key:
-        if root.left != None:
-            return Find(key, root.left)
+class Vertex:
+    def __init__(self, key, sum, left, right, parent):
+        (self.key, self.sum, self.left, self.right, self.parent) = (key, sum, left, right, parent)
+
+def update(v):
+    if v == None:
+        return
+    v.sum = v.key + ((v.left.sum if v.left != None else 0)) + ((v.right.sum if v.right != None else 0))
+    if v.left != None:
+        v.left.parent = v
+    if v.right != None:
+        v.right.parent = v
+
+def smallRotation(v):
+    parent = v.parent
+    if parent == None:
+        return
+    grandparent = v.parent.parent
+    if parent.left == v:
+        m = v.right
+        v.right = parent
+        parent.left = m
+    else:
+        m = v.left
+        v.left = parent
+        parent.right = m
+    update(parent)
+    update(v)
+    v.parent = grandparent
+    if grandparent != None:
+        if grandparent.left == parent:
+            grandparent.left = v
         else:
-            return root
-    elif key > root.key:
-        if root.right != None:
-            return Find(key, root.right)
+            grandparent.right = v
+
+def bigRotation(v):
+    if v.parent.left == v and v.parent.parent.left == v.parent:
+        smallRotation(v.parent)
+        smallRotation(v)
+    elif v.parent.right == v and v.parent.parent.right == v.parent:
+        smallRotation(v.parent)
+        smallRotation(v)
+    else:
+        smallRotation(v)
+        smallRotation(v)
+
+def splay(v):
+    if v == None:
+        return None
+    while v.parent != None:
+        if v.parent.parent == None:
+            smallRotation(v)
+            break
+        bigRotation(v)
+    return v
+
+def find(root, key):
+    v = root
+    last = root
+    next = None
+    while v != None:
+        if v.key >= key and (next == None or v.key < next.key):
+            next = v
+        last = v
+        if v.key == key:
+            break
+        if v.key < key:
+            v = v.right
         else:
-            return root
-    return root
+            v = v.left
+    root = splay(last)
+    return (next, root)
+
+def split(root, key):
+    (result, root) = find(root, key)
+    if result == None:
+        return (root, None)
+    right = splay(result)
+    left = right.left
+    right.left = None
+    if left != None:
+        left.parent = None
+    update(left)
+    update(right)
+    return (left, right)
+
+def merge(left, right):
+    if left == None:
+        return right
+    if right == None:
+        return left
+    while right.left != None:
+        right = right.left
+    right = splay(right)
+    right.left = left
+    update(right)
+    return right
+
+root = None
+
+def insert(x):
+    global root
+    (left, right) = split(root, x)
+    new_vertex = None
+    if right == None or right.key != x:
+        new_vertex = Vertex(x, x, None, None, None)
+    root = merge(merge(left, new_vertex), right)
+
+def leftDescendant(n):
+    if (n.left == null):
+        return n
+    else:
+        return leftDescendant(n.left)
+
+def rightAncestor(n):
+    if n.key < n.parent.key:
+        return n.parent
+    else:
+        return rightAncestor(n.parent)
 
 def Next(node):
     if node.right != None:
-        return LeftDescendants(node.right)
+        return leftDescendant(node.right)
     else:
-        return RightAncestors(node)
+        return rightAncestor(node)
 
-def LeftDescendants(node):
-    if node.left == None:
-        return node
+def deleteNode(node):
+    global root
+    if node.right == None:
+        root = node.left
     else:
-        return LeftDescendants(node.left)
+        temp = Next(node)
+        node = temp
+        root = node.right
+        root.left = node.left
+    if (root != None):
+        root.parent = None
+    update(root)
 
-def RightAncestors(node):
-    if node.parent == None:
-        return None
-    if node.key < node.parent.key:
-        return node.parent
-    else:
-        return RightAncestors(node.parent)
-    
-def Insert(key, root):
-    if root == None:
-        return Node(key)
-    if key < root.key:
-        root.left = Insert(key, root.left)
-        root.left.parent = root
-    if key > root.key:
-        root.right = Insert(key, root.right)
-        root.right.parent = root
-    return root
-
-def Delete(key, root):
-    if root == None:
-        return root
-    if key < root.key:
-        root.left = Delete(key, root.left)
-        if root.left != None:
-            root.left.parent = root
-    elif key > root.key:
-        root.right = Delete(key, root.right)
-        if root.right != None:
-            root.right.parent = root
-    else:    
-        if root.left == None:
-            if root.right != None:
-                root.right.parent = root.parent
-            return root.right
-        elif root.right == None:
-            if root.left != None:
-                root.left.parent = root.parent
-            return root.left
-        next_node = Next(root)
-        root.key = next_node.key
-        root.right = Delete(next_node.key, root.right)
-    return root
-
-def AVLInsert(key, root):
-    root = Insert(key, root)
-    node = Find(key, root)
-    root = Rebalance(node)
-    return root
-
-def AVLDelete(key, root):
-    node = Find(key, root)
-    if node.parent != None:
-        P = node.parent
-    root = Delete(key, root)
-    if node.parent != None:
-        root = Rebalance(P)
-    if root != None:
-        if root.left != None:
-            root.left.height = AdjustHeight(root.left)
-        if root.right != None:
-            root.right.height = AdjustHeight(root.right)
-    root = Rebalance(root)
-    return root
-
-def Rebalance(node):
-    if node == None:
-        return node
-    P = node.parent
-    if node.left != None and node.right != None:
-        if node.left.height > node.right.height + 1:
-            node = RebalanceRight(node)
-        elif node.right.height > node.left.height + 1:
-            node = RebalanceLeft(node)
-    elif node.left != None:
-        if node.left.height > 1:
-            node = RebalanceRight(node)
-    elif node.right != None:
-        if node.right.height > 1:
-            node = RebalanceLeft(node)
-    node.height = AdjustHeight(node)
-    if P != None:
-        return Rebalance(P)
-    return node
-    
-def AdjustHeight(node):
-    if node.left == None and node.right == None:
-        return 1
-    if node.left != None and node.right != None:
-        return 1 + max(AdjustHeight(node.left), AdjustHeight(node.right))
-    elif node.left != None:
-        return 1 + AdjustHeight(node.left)
-    elif node.right != None:
-        return 1 + AdjustHeight(node.right)
-    
-def RebalanceLeft(node):
-    R = node.right
-    if R.right != None and R.left != None:
-        if R.left.height > R.right.height:
-            rotated = RotateRight(R)
-    elif R.left != None:
-        rotated = RotateRight(R)
-    rotated = RotateLeft(node)
-    if rotated.left != None:
-        rotated.left.height = AdjustHeight(rotated.left)
-    if rotated.right != None:
-        rotated.right.height = AdjustHeight(rotated.right)
-    rotated.height = AdjustHeight(rotated)
-    return rotated
-
-def RebalanceRight(node):
-    L = node.left
-    if L.left != None and L.right != None:
-        if L.right.height > L.left.height:
-            rotated = RotateLeft(L)
-    elif L.right != None:
-        rotated = RotateLeft(L)
-    rotated = RotateRight(node)
-    if rotated.right != None:
-        rotated.right.height = AdjustHeight(rotated.right)
-    if rotated.left != None:
-        rotated.left.height = AdjustHeight(rotated.left)
-    rotated.height = AdjustHeight(rotated)
-    return rotated
-        
-def RotateLeft(node):
-    R = node.right
-    L = R.left
-    R.left = R.parent
-    R.left.right = L
-    if L != None:
-        L.parent = R.left
-    R.parent = node.parent
-    R.left.parent = R
-    if R.parent != None:
-        if node == R.parent.left:
-            R.parent.left = R
-        elif node == R.parent.right:
-            R.parent.right = R
-    return R
-    
-def RotateRight(node):
-    L = node.left
-    R = L.right
-    L.right = L.parent
-    L.right.left = R
-    if R != None:
-        R.parent = L.right
-    L.parent = node.parent
-    L.right.parent = L
-    if L.parent != None:
-        if node == L.parent.right:
-            L.parent.right = L
-        elif node == L.parent.left:
-            L.parent.left = L
-    return L
-
-def RangeSum(a, b, root):
-    result = []
-    node = Find(a, root)
+def erase(key):
+    global root
+    points = find(root, key + 1)
+    next_node = points[0]
+    if next_node != None:
+        splay(next_node)
+    node = points[1]
     if node != None:
-        while node != None and node.key <= b:
-            if node.key >= a:
-                result.append(node.key)
-            node = Next(node)
-    return sum(result)
+        if node.key == key:
+            deleteNode(node)
+
+def search(key):
+    global root
+    if (root == None):
+        return False
+    if (root.key == key):
+        return True
+    elif (root.key > key):
+        if (root.left == None):
+            return False
+        else:
+            root = root.left
+            return search(key)
+    elif (root.key < key):
+        if (root.right == None):
+            return False
+        else:
+            root = root.right
+            return search(key)
+    return False
+
+def sum(from_point, to_point):
+    global root
+    (left, middle) = split(root, from_point)
+    (middle, right) = split(middle, to_point + 1)
+    answer = 0
+    if (left != None and left.key >= from_point and left.key <= to_point):
+        answer += left.sum
+    if (middle != None and middle.key >= from_point and middle.key <= to_point):
+        answer += middle.sum
+    if (right != None and right.key >= from_point and right.key <= to_point):
+        answer += right.sum
+    root = merge(merge(left, middle), right)
+    return answer
 
 if __name__ == '__main__':
-    tree = None
     M = 1000000001
-    x = 0
     n = int(input())
+    x = 0
     data = input().split()
     i = 0
     operation = []
@@ -219,24 +200,12 @@ if __name__ == '__main__':
             continue
     for j in range(n):
         if operation[j][0] == '+':
-            node = Find((int(operation[j][1]) + x) % M, tree)
-            if node is None or (node is not None and node.key != (int(operation[j][1]) + x) % M):
-                tree = AVLInsert((int(operation[j][1]) + x) % M, tree)
-            else:
-                continue
+            insert((int(operation[j][1]) + x) % M)
         elif operation[j][0] == '-':
-            node = Find((int(operation[j][1]) + x) % M, tree)
-            if node is not None and node.key == (int(operation[j][1]) + x) % M:
-                tree = AVLDelete((int(operation[j][1]) + x) % M, tree)
-            else:
-                continue
+            erase((int(operation[j][1]) + x) % M)
         elif operation[j][0] == '?':
-            node = Find((int(operation[j][1]) + x) % M, tree)
-            if node is not None and node.key == (int(operation[j][1]) + x) % M:
-                print("Found")
-            else:
-                print("Not found")
+            print ('Found' if search((int(operation[j][1]) + x) % M) else 'Not found')
         elif operation[j][0] == 's':
-            summ = RangeSum((int(operation[j][1]) + x) % M, (int(operation[j][2]) + x) % M, tree)
-            x = summ
-            print(summ)
+            result = sum((int(operation[j][1]) + x) % M, (int(operation[j][2]) + x) % M)
+            print(result)
+            x = result % M
