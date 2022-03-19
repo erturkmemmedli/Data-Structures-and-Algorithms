211999f0b1d06d6559b013ff10cf5f5eefec59fd
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-03-19 21:55:23 +0400

    Update hash_chains.py

--- Data Structures/3 - Hash Tables/hash_chains.py
+++ Data Structures/3 - Hash Tables/hash_chains.py
@@ -1,115 +1,51 @@
-class Node:
-    def __init__(self, key):
-        self.key = key
-        self.next = None
+from collections import deque
 
-class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-        
-    def push_front(self, key):
-        node = Node(key)
-        node.next = self.head
-        self.head = node
-        if self.tail == None:
-            self.tail = self.head
-    
-    def pop_front(self):
-        if self.head == None:
-            return
-        else:
-            self.head = self.head.next
-        if self.head == None:
-            self.tail = self.head
-    
-    def find_key(self, key):
-        temp = self.head
-        while(temp != None and temp.key != key):
-            temp = temp.next
-        if self.head == None or temp == None:
-            print('no')
-        else:
-            print('yes')          
+def hash_func(string):
+    hash_value = 0
+    for i in range(len(string)):
+        hash_value = (hash_value + ord(string[i]) * (multiplier ** i)) % prime
+    return hash_value % bucket_count
 
-    def delete_key(self, key):
-        temp = self.head
-        if temp == None:
-            return
-        elif temp.key == key:
-            self.pop_front()
-        else:
-            while(temp.next != None and temp.next.key != key):
-                temp = temp.next
-            if temp.next == None and temp.key != key:
-                return
-            else:
-                if temp.next.next != None:
-                    temp.next = temp.next.next
-                else:
-                    self.tail = temp
-                    temp.next = None
+def addition(string):
+    hash_value = hash_func(string)
+    if string not in bucket[hash_value]:
+        bucket[hash_value].appendleft(string)
+        
+def deletion(string):
+    hash_value = hash_func(string)
+    if string in bucket[hash_value]:
+        bucket[hash_value].remove(string)
     
-    def print_linked_list(self):
-        temp = self.head
-        if temp == None:
-            print()
-        while(temp != None):
-            if temp != self.tail:
-                print(temp.key, end = ' ')
-            else:
-                print(temp.key)
-            temp = temp.next
-        
-class HashChainTable:
-    multiplier = 263
-    prime = 1000000007
-
-    def __init__(self, bucket_count):
-        self.bucket_count = bucket_count
-        self.hash_table = [None] * self.bucket_count
-        for i in range(self.bucket_count) :
-            self.hash_table[i] = LinkedList()
-        
-    def hash_func(self, string):
-        hash_value = 0
-        for i in range(len(string)):
-            hash_value = (hash_value + ord(string[i]) * (self.multiplier ** i)) % self.prime
-        return hash_value % self.bucket_count
+def search(string):
+    hash_value = hash_func(string)
+    print("yes" if string in bucket[hash_value] else "no")
     
-    def insert(self, string):
-        index = self.hash_func(string)
-        self.hash_table[index].push_front(string)
-
-    def find(self, string):
-        index = self.hash_func(string)
-        self.hash_table[index].find_key(string)
-        
-    def delete(self, string):
-        index = self.hash_func(string)
-        self.hash_table[index].delete_key(string)
-                
-    def print_chain(self, index):
-        self.hash_table[index].print_linked_list()
+def print_bucket(index):
+    if index not in bucket:
+        print()
+    else:
+        print(' '.join(bucket[index]))
 
 if __name__ == '__main__':
+    global multiplier, prime, bucket_count, bucket
+    multiplier = 263
+    prime = 1000000007
+    
     bucket_count = int(input())
-    chain = HashChainTable(bucket_count)
-    elements = []
+    bucket = {}
+    for i in range(bucket_count):
+        bucket[i] = deque()
+    
     query_count = int(input())
-    for i in range(query_count):
-        query = input().split()
-        if query[0] == 'add':
-            if query[1] not in elements:
-                chain.insert(query[1])
-                elements.append(query[1])
-        elif query[0] == 'del':
-            if query[1] in elements:
-                chain.delete(query[1])
-                elements.remove(query[1])
-        elif query[0] == 'find':
-            chain.find(query[1])
-        elif query[0] == 'check':
-            chain.print_chain(int(query[1]))
-        else:
-            assert(0)
+    queries = [input() for _ in range(query_count)]
+        
+    for query in queries:
+        operation, argument = query.split()
+        if operation == 'add':
+            addition(argument)
+        if operation == 'del':
+            deletion(argument)
+        if operation == 'find':
+            search(argument)
+        if operation == 'check':
+            print_bucket(int(argument))
