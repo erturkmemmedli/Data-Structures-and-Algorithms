7d7b15af45199d9a77d293fce3f5979d30aac60f
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2022-03-30 15:44:51 +0400

    Add files via upload

---
+++ Algorithms on Graphs/breadth_first_search.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+def BFS(adjacency_list, start, end):
+    distance = [float('inf') for _ in range(len(adjacency_list))]
+    distance[start] = 0
+    Q = deque([start])
+    while len(Q) != 0:
+        vertex = Q.popleft()
+        for path in adjacency_list[vertex]:
+            if distance[path] == float('inf'):
+                Q.append(path)
+                distance[path] = distance[vertex] + 1
+                if path == end:
+                    return distance[path]
+    return -1
+
+if __name__ == '__main__':
+    vertex, edge = map(int, input().split())
+    edges = [tuple(map(int, input().split())) for _ in range(edge)]
+    start, end = map(int, input().split())
+    start, end = start - 1, end - 1
+    adjacency_list = [[] for _ in range(vertex)]
+    for a, b in edges:
+        adjacency_list[a - 1].append(b - 1)
+        adjacency_list[b - 1].append(a - 1)
+    print(BFS(adjacency_list, start, end))
