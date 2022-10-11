f238c471c54e5b7b15b10dd74eee22cade0f1158
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-10-11 11:58:02 +0400

    Create union_find1.py

---
+++ Some Algorithms/union_find1.py
@@ -0,0 +1,22 @@
+self.parent = [-1] * (n+1)
+self.rank = [0] * (n+1)
+        
+def find(self, x):
+    if self.parent[x] == -1:
+        return x
+    self.parent[x] = self.find(self.parent[x])
+    return self.parent[x]
+
+def union(self, a, b):
+    root_a = self.find(a)
+    root_b = self.find(b)
+    if root_a == root_b:
+        return False
+    elif root_a < root_b:
+        self.parent[root_a] = root_b
+        self.rank[root_b] += 1
+        return True
+    else:
+        self.parent[root_b] = root_a
+        self.rank[root_a] += 1
+        return True
