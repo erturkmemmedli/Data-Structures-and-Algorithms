e2b2337f5dd6f269b9543d1233e6a3dae93b4053
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-12-11 17:41:43 +0400

    Update Q1_key_value_pair_database_with_transactions.py

--- Some Algorithms/Q1_key_value_pair_database_with_transactions.py
+++ Some Algorithms/Q1_key_value_pair_database_with_transactions.py
@@ -16,3 +16,60 @@ database transactions by also implementing these commands:
 * `COMMIT` – Close **all** open transactions, permanently applying the changes
   made in them. Returns an error if no transaction is in progress.
 """
+
+class KVPDatabase:
+    def __init__(self):
+        self.database = {}
+        self.operations = []
+        
+    def set(self, name, value):
+        if not self.operations:
+            self.database[name] = value
+        else:
+            self.operations[-1].append((1, name, value))
+        
+    def get(self, name):
+        result = "NULL"
+        if self.operations:
+            for operation in self.operations:
+                for command, key, val in operation:
+                    if key == name:
+                        if command == 1:
+                            result = val
+                        else:
+                            result = "NULL"
+            return result
+        else:
+            return self.database[name] if name in self.database else result
+    
+    def unset(self, name):
+        if not self.operations:
+            if name in self.database:
+                del self.database[name]
+        else:
+            self.operations[-1].append((0, name, None))
+        
+    def begin(self):
+        self.operations.append([])
+        
+    def rollback(self):
+        if self.operations:
+            self.operations.pop()
+        else:
+            raise Exception("There is no transaction")
+        
+    def commit(self):
+        if self.operations:
+            for operation in self.operations:
+                for command, key, val in operation:
+                    if command == 1:
+                        self.database[key] = val
+                    else:
+                        del self.database[key]
+        self.operations = []
+        else:
+            raise Exception("There is no transaction")
+        
+    def test(self):
+        print(self.database)
+        print(self.operations)
