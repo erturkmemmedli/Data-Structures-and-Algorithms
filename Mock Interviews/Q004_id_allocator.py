6d0e6748a02ce679063a35eb138be759d83fe459
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2022-12-11 18:23:59 +0400

    Update Q004_id_allocator.py

--- Mock Interviews/Q004_id_allocator.py
+++ Mock Interviews/Q004_id_allocator.py
@@ -11,6 +11,44 @@ Client:
         int id1 = idAlloc.allocate(); // 0
         int id2 = idAlloc.allocate(); // 1
 
-        idAlloc.isAllocated( 5 ); false
+        idAlloc.isAllocated( 5 ); // false
     }
 '''
+
+# We assume release(id) call is always valid.
+
+# Trade-Off 1: Checking whether ID is allocated Time: O(n); Space: O(1).
+class IdAllocator:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.freeIDs = list(range(capacity))
+        
+    def allocate(self):
+        return self.freeIDs.pop()
+    
+    def release(self, id):
+        self.freeIDs.append(id) 
+    
+    def isAllocated(self, id):
+        for num in self.freeIDs:
+            if num == id: return True
+        return False
+    
+# Trade-Off 2: Checking whether ID is allocated Time: O(1); Space: O(n).
+class IdAllocator:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.freeIDs = list(range(capacity))
+        self.usedIDs = set()
+        
+    def allocate(self):
+        allocated = self.freeIDs.pop()
+        self.usedIDs.add(allocated)
+        return allocated
+    
+    def release(self, id):
+        self.usedIDs.remove(id)
+        self.freeIDs.append(id)
+    
+    def isAllocated(self, id):
+        return id in self.usedIDs
