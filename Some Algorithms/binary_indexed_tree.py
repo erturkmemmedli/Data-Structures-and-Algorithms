0a108765a7254ab4cab673b6c058a33e48be722a
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-11 14:29:28 +0400

    Create binary_indexed_tree.py

---
+++ Some Algorithms/binary_indexed_tree.py
@@ -0,0 +1,18 @@
+class BinaryIndexedTree(object):
+    def __init__(self, n):
+        self.sums = [0] * (n + 1)
+
+    def update(self, i, val):
+        while i < len(self.sums):
+            self.sums[i] += 1
+            i += i & -i
+
+    def sum(self, i):
+        r = 0
+        while i > 0:
+            r += self.sums[i]
+            i -= i & -i
+        return r
+
+# hashTable = {v: i for i, v in enumerate(sorted(set(nums)))}
+# tree = BinaryIndexedTree(len(hashTable))
