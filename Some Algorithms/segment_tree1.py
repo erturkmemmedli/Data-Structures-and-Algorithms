852c8f32398a4fcdde0434c3d76c7fd071e5bc62
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2023-01-19 11:32:58 +0400

    Update segment_tree1.py

--- Some Algorithms/segment_tree1.py
+++ Some Algorithms/segment_tree1.py
@@ -11,13 +11,13 @@ class SegmentTree:
         if l == tl and r == tr:
             self.cnts[v] += h
         else:
-            tm = (tl + tr)//2
-            self.update(v*2, tl, tm, l, min(r, tm), h)
-            self.update(v*2+1, tm+1, tr, max(l, tm+1), r, h)
+            tm = (tl + tr) // 2
+            self.update(v * 2, tl, tm, l, min(r, tm), h)
+            self.update(v * 2 + 1, tm + 1, tr, max(l, tm + 1), r, h)
 
         if self.cnts[v] > 0:
             self.total[v] = self.xs[tr + 1] - self.xs[tl]
         else:
-            self.total[v] = self.total[v*2] + self.total[v*2+1]
+            self.total[v] = self.total[v * 2] + self.total[v * 2 + 1]
 
         return self.total[v]
