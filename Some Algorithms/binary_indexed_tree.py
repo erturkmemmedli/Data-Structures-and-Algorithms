46a0b9678176a997f56c63d04b95af8b6a79b6b3
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2024-01-22 13:50:13 +0400

    Update binary_indexed_tree.py

--- Some Algorithms/binary_indexed_tree.py
+++ Some Algorithms/binary_indexed_tree.py
@@ -1,18 +1,37 @@
+'''
+To update tree, we need to update next element step-by-step. Procedure is as follows:
+    1. Find 2's complement of index -> -idx
+    2. AND it with original index -> idx & -idx
+    3. Add them together to get next index -> idx + (idx & -idx)
+
+To find sum until given index, we need to traverse toward root node. Procedure is as follows:
+    1. Find 2's complement of index -> -idx
+    2. AND it with original index -> idx & -idx
+    3. Subtract them to find parent index -> idx - (idx & -idx)
+'''
+
 class BinaryIndexedTree(object):
     def __init__(self, n):
-        self.sums = [0] * (n + 1)
+        self.tree = [0] * (n + 1)
 
-    def update(self, i, val):
-        while i < len(self.sums):
-            self.sums[i] += 1
-            i += i & -i
+    def sum(self, idx):
+        result = 0
+        while idx > 0:
+            result += self.tree[idx]
+            idx -= idx & -idx
+        return result
+    
+    def update(self, idx, val):
+        while idx < len(self.tree):
+            self.tree[idx] += val
+            idx += idx & -idx
 
-    def sum(self, i):
-        r = 0
-        while i > 0:
-            r += self.sums[i]
-            i -= i & -i
-        return r
 
-# hashTable = {v: i for i, v in enumerate(sorted(set(nums)))}
-# tree = BinaryIndexedTree(len(hashTable))
+# arr = [3,1,2,5,6,5,2,-3,4,8]
+# bit = BinaryIndexedTree(len(arr))
+
+# for i, val in enumerate(arr):
+#     bit.update(i + 1, val)
+
+# print(bit.tree)
+# print(bit.sum(7))
