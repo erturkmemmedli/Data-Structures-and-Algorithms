b15b9e97ab8736dde7d80631afc7d750fffdc5db
Arturk Mammadli <49902768+erturkmemmedli@users.noreply.github.com>
2024-01-27 19:34:17 +0400

    Update segment_tree.py

--- Some Algorithms/segment_tree.py
+++ Some Algorithms/segment_tree.py
@@ -7,9 +7,10 @@ class SegmentTree:
         self.array = array
         self.length = len(array)
         self.segment_length = 2 * 2 ** math.ceil(math.sqrt(self.length)) - 1
-        self.segment_tree = [math.inf for _ in range(self.segment_length)]
+        self.segment_tree = [math.inf] * self.segment_length
+        self.lazy = [0] * self.segment_length
         
-    def build_segment_tree(self, low: int = 0, high: int = None, pos: int = 0, op: Callable = None) -> None:
+    def build(self, low: int = 0, high: int = None, pos: int = 0, op: Callable = None) -> None:
         if high == None:
             high = self.length - 1
 
@@ -18,21 +19,60 @@ class SegmentTree:
             return
 
         mid = low + (high - low) // 2
-        self.build_segment_tree(low, mid, pos * 2 + 1, op)
-        self.build_segment_tree(mid + 1, high, pos * 2 + 2, op)
-
-        self.segment_tree[pos] = op([self.segment_tree[pos * 2 + 1], self.segment_tree[pos * 2 + 2]])   
         
-    def range_query(self, left, right, low = 0, high = None, pos = 0, op: Callable = None, factor: float = None) -> Union[int, float]:
+        self.build(low, mid, pos * 2 + 1, op)
+        self.build(mid + 1, high, pos * 2 + 2, op)
+
+        self.segment_tree[pos] = op([self.segment_tree[pos * 2 + 1], self.segment_tree[pos * 2 + 2]])
+        
+    def update(self, left: int, right: int, delta: int, low: int = 0, high: int = None, pos: int = 0, op: Callable = None) -> None:
         if high == None:
             high = self.length - 1
+            
+        if self.lazy[pos] != 0:
+            self.segment_tree[pos] += self.lazy[pos]
+            if low != high:
+                self.lazy[pos * 2 + 1]  += self.lazy[pos]
+                self.lazy[pos * 2 + 2] += self.lazy[pos]
+            self.lazy[pos] = 0
+            
+        if low >= left and high <= right: # total overlap
+            self.segment_tree[pos] += delta
+            if low != high:
+                self.lazy[pos * 2 + 1]  += self.lazy[pos]
+                self.lazy[pos * 2 + 2] += self.lazy[pos]
+            return
+        
+        if high < left or low > right: # no overlap
+            return
+        
+        mid = low + (high - low) // 2
+        
+        self.update(left, right, delta, low, mid, pos * 2 + 1, op)
+        self.update(left, right, delta, mid + 1, high, pos * 2 + 2, op)
+
+        self.segment_tree[pos] = op([self.segment_tree[pos * 2 + 1], self.segment_tree[pos * 2 + 2]])
+        
+        
+    def range_query(self, left: int, right: int, low: int = 0, high: int = None, pos: int = 0, op: Callable = None, factor: float = None) -> Union[int, float]:
+        if high == None:
+            high = self.length - 1
+
+        if self.lazy[pos] != 0:
+            self.segment_tree[pos] += self.lazy[pos]
+            if low != high:
+                self.lazy[pos * 2 + 1]  += self.lazy[pos]
+                self.lazy[pos * 2 + 2] += self.lazy[pos]
+            self.lazy[pos] = 0
         
         if low >= left and high <= right: # total overlap
             return self.segment_tree[pos]
+        
         if high < left or low > right: # no overlap
             return factor
         
         mid = low + (high - low) // 2
+        
         l = self.range_query(left, right, low, mid, pos * 2 + 1, op, factor)
         r = self.range_query(left, right, mid + 1, high, pos * 2 + 2, op, factor)
         
@@ -40,42 +80,51 @@ class SegmentTree:
       
     
 class SegmentTreeMinimum(SegmentTree):
-    def build_segment_tree(self, low = 0, high = None, pos = 0, op = None) -> None:
-        super().build_segment_tree(low, high, pos, min)
-
+    def build(self, low: int = 0, high: int = None, pos: int = 0, op: Callable = None) -> None:
+        super().build(low, high, pos, min)
+    
+    def update(self, left: int, right: int, delta: int, low: int = 0, high: int = None, pos: int = 0, op: Callable = None) -> None:
+        super().update(left, right, delta, low, high, pos, min)
+        
     def range_query(self, left, right, low = 0, high = None, pos = 0, op = None, factor = None):
         return super().range_query(left, right, low, high, pos, min, math.inf)
 
 
 class SegmentTreeMaximum(SegmentTree):
-    def build_segment_tree(self, low = 0, high = None, pos = 0, op = None) -> None:
-        super().build_segment_tree(low, high, pos, max)
-
+    def build(self, low: int = 0, high: int = None, pos: int = 0, op: Callable = None) -> None:
+        super().build(low, high, pos, max)
+    
+    def update(self, left: int, right: int, delta: int, low: int = 0, high: int = None, pos: int = 0, op: Callable = None) -> None:
+        super().update(left, right, delta, low, high, pos, max)
+        
     def range_query(self, left, right, low = 0, high = None, pos = 0, op = None, factor = None):
         return super().range_query(left, right, low, high, pos, max, -math.inf)
       
     
 class SegmentTreeSum(SegmentTree):
-    def build_segment_tree(self, low = 0, high = None, pos = 0, op = None) -> None:
-        super().build_segment_tree(low, high, pos, sum)
-
+    def build(self, low: int = 0, high: int = None, pos: int = 0, op: Callable = None) -> None:
+        super().build(low, high, pos, sum)
+    
+    def update(self, left: int, right: int, delta: int, low: int = 0, high: int = None, pos: int = 0, op: Callable = None) -> None:
+        super().update(left, right, delta, low, high, pos, sum)
+        
     def range_query(self, left, right, low = 0, high = None, pos = 0, op = None, factor = None):
         return super().range_query(left, right, low, high, pos, sum, 0)
 
 
-# arr = [0, 2, 4, -1, 8, -3, 1, 5]
+arr = [0, 2, 4, -1, 8, -3, 1, 5]
 
-# s = SegmentTreeMinimum(arr)
-# s.build_segment_tree()
-# print(s.segment_tree)
-# print(s.range_query(0, 4))
+s = SegmentTreeMinimum(arr)
+s.build()
+print(s.segment_tree)
+print(s.range_query(0, 4))
 
-# t = SegmentTreeMaximum(arr)
-# t.build_segment_tree()
-# print(t.segment_tree)
-# print(t.range_query(1, 7))
+t = SegmentTreeMaximum(arr)
+t.build()
+print(t.segment_tree)
+print(t.range_query(1, 7))
 
-# z = SegmentTreeSum(arr)
-# z.build_segment_tree()
-# print(z.segment_tree)
-# print(z.range_query(0, 3))
+z = SegmentTreeSum(arr)
+z.build()
+print(z.segment_tree)
+print(z.range_query(0, 3))
